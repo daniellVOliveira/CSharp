56b88acb6211491a5b2b1326115683c7288fc34d
DanielOliveira <danielvitor76@gmail.com>
2018-07-31 21:27:13 -0300

    Converter String para Int e Int Parse

--- Aulas/ConsoleApp1/Program.cs
+++ Aulas/ConsoleApp1/Program.cs
@@ -8,11 +8,22 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        public static int Id { get; set; }
+
         static void Main(string[] args)
         {
 
             Console.WriteLine("Hello World");
             Console.ReadKey();
+
+            Console.WriteLine("Digite um número");
+            Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Você digitou: " + Id);
+
+            Console.ReadKey();
+
         }
     }
 }
