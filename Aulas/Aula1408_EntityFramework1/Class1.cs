89e296c2b14d18e04a1c8dfa130c4424444238bc
DanielOliveira <danielvitor76@gmail.com>
2018-08-14 21:30:19 -0300

    Criando projeto Aula1408_EntitiyFramework1 (conceitos e instalação)

---
+++ Aulas/Aula1408_EntityFramework1/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1408_EntityFramework1
+{
+    public class Class1
+    {
+
+        /* Conceitos:
+          ORM é uma técnica de mapeamento cuja a principal proposta é suprir as disparidades entre 
+          o modelo relacional e o modelo orientado a objetos. Com esta abordagem, é possível trabalhar 
+          com a construção de um sistema utilizando Orientação a Objetos, cujo os objetos são persistidos
+          em um banco de dados relacional.
+          Outra grande importância do ORM é que muitos sistemas ainda precisam persistir dados de algum
+          lugar onde a base de dados é relacional, algo que ainda está em peso no mercado. Utilizando um 
+          Framework em ORM, é possível economizar várias linhas de códigos relacionadas a conexão com o 
+          Banco de Dados, já que não será necessário escrever todo o código SQL.
+          
+        Instalando o Entity Framework
+          *
+          * 
+          * 
+         */
+    }
+}
