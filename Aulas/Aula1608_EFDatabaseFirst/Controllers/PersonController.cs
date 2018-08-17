3f281a76228e4fca03030aa840668b7c33bed244
DanielOliveira <danielvitor76@gmail.com>
2018-08-16 22:01:57 -0300

    Criação do banco e algumas funções no CRUD utilizando o Entity Framework.

---
+++ Aulas/Aula1608_EFDatabaseFirst/Controllers/PersonController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1608_EFDatabaseFirst.Controllers
+{
+    class PersonController
+    {
+        void Inserir(Person p)
+        {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            contexto.Person.Add(p);
+            contexto.SaveChanges();
+
+        }
+
+        List<Person> ListarTodosPerson() {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.ToList();
+        }
+
+        Person BuscarPorId(int id) {
+            AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+            return contexto.Person.Find(id); //Metodo Find só faz pesquisa pela Chave!
+        }
+
+        void Excluir(int id)
+        {
+            
+            Person pExcluir = BuscarPorId(id);
+
+            if (pExcluir != null) {
+                AdventureWorks2016Entities contexto = new AdventureWorks2016Entities();
+                contexto.Person.Remove(pExcluir);
+                contexto.SaveChanges();
+            }
+        }
+
+    }
+}
