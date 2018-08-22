31416b18c12f23f4e02c52d71b43c14c1fa3c1dc
DanielOliveira <danielvitor76@gmail.com>
2018-08-21 21:44:48 -0300

    Criando estrutura da classe Produto(modelo)

---
+++ Aulas/Aula2108_CodeFirst/Modelos/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula2108_CodeFirst.Modelos
+{
+    class Produto
+    {
+        public int ProdutoID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Preco { get; set; }
+
+        public decimal Imposto { get; set; }
+
+        //Relacionamento Produto <--> Categoria
+
+        public int CategoriaID { get; set; } //Chave estrangeira da Tabela **O NOME PRECISA BATER COM A OUTRA TABELA** 
+
+        public virtual Categoria _Categoria { get; set; } //Propriedade de navegação
+    }
+
+
+}
