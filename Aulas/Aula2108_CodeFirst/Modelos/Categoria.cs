1b21bf1ebc5b2120a506fcdd30fd034707601337
DanielOliveira <danielvitor76@gmail.com>
2018-08-21 21:34:11 -0300

    Escrevendo estrutura da classe Categoria (modelo)

--- Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,14 @@ namespace Aula2108_CodeFirst.Modelos
 {
     class Categoria
     {
+        //Chave Primária é reconhecida se usar a palavra Id ou nomeClasseID
+        [Key] //Usado quando não é utilizado nenhuma nomenclatura reconhecida pelo Entity
+        public int CategoriaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
 
     }
 }
