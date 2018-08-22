06dcd4e1d06c045dda1c8facc57ce06857f4b9e2
DanielOliveira <danielvitor76@gmail.com>
2018-08-21 22:01:42 -0300

    Herdando DbContext(Entity)

--- Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
+++ Aulas/Aula2108_CodeFirst/Modelos/DAL/Contexto.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Aula2108_CodeFirst.Modelos.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext //Classe contexto deve herdar a classe DbContext (Entity)
     {
 
     }
